c9ecb211ea811d99444d5882d37da8d2fd68cf56
gueyemo <m.gueye0411@gmail.com>
2020-01-27 14:21:40 +0100

    the beginning of everything :)

---
+++ vue/Acceuilview.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>Le Coin Etudiant</title>
+  <link rel="stylesheet" href="../vue/bootstrap.min.css">
+</head>
+<body>
+  <header>
+    <?php include('header.php') ?>
+  </header>
+
+Coucou jerome, a toi de jouer
+</body>
+</html>
