89cc4dc360edd428f7fd639de6aed5ee04fd5528
gueyemo <m.gueye0411@gmail.com>
2020-02-06 19:32:23 +0100

    Modification de la page d'accueil et création d'un dossier pour le css

--- controleur/Accueil.php
+++ controleur/Accueil.php
@@ -1,4 +1,4 @@
 <?php
 // require_once('../modele/ProduitclassDAO.php');// inclus la class Produit DAO
-require_once('../vue/Acceuilview.php'); //On inclue la vue de l'acceuil (modele MVC)
+require_once('../vue/Accueilview.php'); //On inclue la vue de l'acceuil (modele MVC)
 ?>
--- vue/Accueilview.php
+++ vue/Accueilview.php
@@ -3,9 +3,9 @@
 <head>
   <meta charset="utf-8">
   <title>Le Coin Etudiant</title>
-  <link rel="stylesheet" href="../vue/script/bootstrap.min.css">
-  <link rel="stylesheet" href="../vue/style.css">
-  <link rel="stylesheet" href='../vue/script/jquery.flipster.css'>
+  <link rel="stylesheet" href="../vue/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../vue/css/Accueilview.css">
+  <link rel="stylesheet" href='../vue/css/jquery.flipster.css'>
 
 
 </head>
@@ -18,9 +18,9 @@
     <img src="../modele/img/loading.gif" alt="Loading..."  />
   </div>
 
-  <article id="carousel">
+  <article id="carousel" class="jumbotron vertical-center" style="background:transparent; padding-top:0;">
 
-    <h2 class="text-center">Les Dernieres Annonces</h2>
+    <h2 class="text-center" style="margin:1em 0"><p><img src="../modele/img/title.gif" alt="Les Dernieres Annonces"> </p></h2>
 
     <div id="carousel" class="coverflow-slider">
       <ul class="flip-items">
@@ -209,7 +209,7 @@
           </div>
         </li>
 
-        <li data-flip-title="Autres">
+        <li data-flip-tit="Autres">
           <div class="img-box adv-img adv-img-half-content" data-anima="fade-left" data-trigger="hover">
             <a href="#" class="img-box anima-scale-up anima">
               <img src="../modele/img/text14.gif">
@@ -226,6 +226,55 @@
       </ul>
     </div>
 
+<article id="formulaire" class="row d-flex justify-content-center">
+    <form style="max-width:50%" class="col-md-6" >
+      <h1 class="text-center">INSCRIPTION</h1>
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="inputEmail4">Email</label>
+          <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+        </div>
+        <div class="form-group col-md-6">
+          <label for="inputPassword4">Password</label>
+          <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="inputAddress">Address</label>
+        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+      </div>
+      <div class="form-group">
+        <label for="inputAddress2">Address 2</label>
+        <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+      </div>
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="inputCity">City</label>
+          <input type="text" class="form-control" id="inputCity">
+        </div>
+        <div class="form-group col-md-4">
+          <label for="inputState">State</label>
+          <select id="inputState" class="form-control">
+            <option selected>Choose...</option>
+            <option>...</option>
+          </select>
+        </div>
+        <div class="form-group col-md-2">
+          <label for="inputZip">Zip</label>
+          <input type="text" class="form-control" id="inputZip">
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="gridCheck">
+          <label class="form-check-label" for="gridCheck">
+            Check me out
+          </label>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
+  </article>
 
 
     <script type="text/javascript">
@@ -235,13 +284,14 @@
     });
     </script>
 
-    <script src="../vue/script/jquery.min.js"></script>
+    <!-- <script src="../vue/script/jquery.min.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src='../vue/script/jquery.flipster.min.js'></script>
     <script>
     var carousel = $('#carousel').flipster({
       style: 'carousel',
       spacing: -0.3,
-      nav: true,
+      nav: false,
       buttons:   true,
       loop: true,
       autoplay: 3000,
