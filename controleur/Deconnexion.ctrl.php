19a7a09d918e0caecac0164897a7b03b6ab7162f
gueyemo <m.gueye0411@gmail.com>
2020-02-22 13:44:09 +0100

    regle probleme reconnexion après deconnexion. Fixes #21

--- controleur/Deconnexion.ctrl.php
+++ controleur/Deconnexion.ctrl.php
@@ -5,7 +5,9 @@ $_SESSION['Connexion'] = false;
 $_SESSION = false;
 
 session_destroy();
-include('../vue/Accueilview.php');
+header('Location:../controleur/Accueil.ctrl.php');
+
+// include('../vue/Accueilview.php');
 // include_once("../controleur/Accueil.ctrl.php");
 
  ?>
